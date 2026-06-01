3a069d7ba4f494c616d4a252467d62f3aa5eea7f
JacsoAron0803 <jacsoaron@gmail.com>
2026-06-01 08:26:38 +0200

    textblock hozzáadás, classok hozzáadásaAdd project files.

---
+++ KisboltKeszletkezelo/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisboltKeszletkezelo.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Nev { get; set; }
+        public string Leiras { get; set; }
+        public decimal Ar { get; set; }
+        public int Mennyiseg { get; set; }
+
+        public Product(string nev, string leiras, decimal ar, int mennyiseg)
+        {
+            Nev = nev;
+            Leiras = leiras;
+            Ar = ar;
+            Mennyiseg = mennyiseg;
+        }
+    }
+}
---
+++ KisboltKeszletkezelo/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisboltKeszletkezelo.Models
+{
+    public class User
+    {
+
+        public User()
+        {
+        }
+        public int Id { get; set; }
+        public string FelhasznaloNev { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public User(string felhasznaloNev, string email, string password, string role)
+        {
+            FelhasznaloNev = felhasznaloNev;
+            Email = email;
+            Password = password;
+            Role = role;
+        }
+    }
+}
